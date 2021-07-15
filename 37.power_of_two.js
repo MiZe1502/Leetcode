e544896fd91ec80743b69399f4c0034830925e70
MiZe1502 <z.com@mail.ru>
2021-07-15 23:11:43 +0300

    power of two

---
+++ 37.power_of_two.js
@@ -0,0 +1,25 @@
+// Given an integer n, return true if it is a power of two. Otherwise, return false.
+// An integer n is a power of two, if there exists an integer x such that n == 2x.
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function(n) {
+    if (n === 1) {
+        return true;
+    }
+
+    if (n < 2) {
+        return false;
+    }
+
+    while (n >= 2) {
+        if (n % 2 !== 0) {
+            return false;
+        }
+        n = n / 2;
+    }
+
+    return true;
+};
